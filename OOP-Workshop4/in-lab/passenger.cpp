d29b789c208543fe0315bf421c71989dee095fe0
Dominic Christoph Nunes <dcnunes@matrix.senecac.on.ca>
2017-02-12 07:52:50 -0500

    working on final error fix for in-lab 4

--- OOP-Workshop4/in-lab/passenger.cpp
+++ OOP-Workshop4/in-lab/passenger.cpp
@@ -11,31 +11,31 @@ namespace holiday {
 
   // TODO: add the default constructor here
   Passenger::Passenger() {
-      m_name[0] = '\0';
-      m_destination[0] = '\0';
-      m_departureYear = 0;
-      m_departureMonth = 0;
-      m_departureDay = 0;
+    m_name[0] = '\0';
+    m_destination[0] = '\0';
+    m_departureYear = 0;
+    m_departureMonth = 0;
+    m_departureDay = 0;
 
   }
 
   // TODO: add the constructor with 2 parameters here
   Passenger::Passenger(const char *name, const char *destination) {
-      if (name == nullptr || name[0] == '\0') {
-          m_name[0] = '\0';
-      } else {
-          strcpy(m_name, name);
-      }
+    if (name == nullptr || name[0] == '\0') {
+      m_name[0] = '\0';
+    } else {
+      strcpy(m_name, name);
+    }
 
-      if (destination == nullptr || destination[0] == '\0') {
-          m_destination[0] = '\0';
-      } else {
-          strcpy(m_destination, destination);
-      }
+    if (destination == nullptr || destination[0] == '\0') {
+      m_destination[0] = '\0';
+    } else {
+      strcpy(m_destination, destination);
+    }
 
-      m_departureYear = 1;
-      m_departureMonth = 7;
-      m_departureDay = 2017;
+    m_departureYear = 1;
+    m_departureMonth = 7;
+    m_departureDay = 2017;
   }
 
   Passenger::~Passenger() {
@@ -44,40 +44,40 @@ namespace holiday {
 
   // TODO: add the canTravelWith(...) function here
   bool Passenger::canTravelWith(const Passenger &passenger) const {
-      bool ret = false;
-      if (m_departureYear == passenger.m_departureYear && m_departureMonth == passenger.m_departureMonth &&
-          m_departureDay == passenger.m_departureDay && m_destination == passenger.m_destination) {
-          ret = true;
-      }
-      return ret;
+    bool ret = false;
+    if (m_departureYear == passenger.m_departureYear && m_departureMonth == passenger.m_departureMonth &&
+        m_departureDay == passenger.m_departureDay && m_destination == passenger.m_destination) {
+      ret = true;
+    }
+    return ret;
   }
 
   // TODO: add the isEmpty() function here
   bool Passenger::isEmpty() const {
-      bool ret = false;
-      if (this->m_name[0] == '\0' || this->m_destination[0] == '\0' || this->m_departureYear == 0 ||
-          this->m_departureMonth == 0 || this->m_departureDay == 0) {
-          ret = true;
-      }
-      return ret;
+    bool ret = false;
+    if (this->m_name[0] == '\0' || this->m_destination[0] == '\0' || this->m_departureYear == 0 ||
+        this->m_departureMonth == 0 || this->m_departureDay == 0) {
+      ret = true;
+    }
+    return ret;
   }
 
   // below is the member function already provided
   // TODO: read it and understand how it accomplishes its task
   void Passenger::display(bool nameOnly) const {
-      if (false == this->isEmpty()) {
-          cout << this->m_name;
-          if (false == nameOnly) {
-              cout << " will travel to " << this->m_destination << ". "
-                   << "The journey will start on "
-                   << this->m_departureYear << "-"
-                   << this->m_departureMonth << "-"
-                   << this->m_departureDay
-                   << "." << endl;
-          }
-      } else {
-          cout << "Invalid passenger!" << endl;
+    if (false == this->isEmpty()) {
+      cout << this->m_name;
+      if (false == nameOnly) {
+        cout << " will travel to " << this->m_destination << ". "
+          << "The journey will start on "
+          << this->m_departureDay << "-"
+          << this->m_departureMonth << "-"
+          << this->m_departureYear 
+          << "." << endl;
       }
+    } else {
+      cout << "Invalid passenger!" << endl;
+    }
   }
 
 
