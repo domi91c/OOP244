f51edd404e52dadbb28ebca133869cfee79ddd16
domi91c <dominic.n@me.com>
2017-02-13 18:22:29 -0500

    working on at_home 4

---
+++ OOP-Workshop4/at-home/passenger.cpp
@@ -0,0 +1,91 @@
+// TODO: add your headers here
+#include <iostream>
+#include "passenger.h"
+#include <cstring>
+
+// TODO: add the namespaces that you will be using here
+using namespace std;
+
+// TODO: holiday namespace here
+namespace holiday {
+
+  // TODO: add the default constructor here
+  Passenger::Passenger() {
+      m_name[0] = '\0';
+      m_destination[0] = '\0';
+      m_departureYear = 0;
+      m_departureMonth = 0;
+      m_departureDay = 0;
+
+  }
+
+  // TODO: add the constructor with 2 parameters here
+  Passenger::Passenger(const char *name, const char *destination) {
+      if (name == nullptr || name[0] == '\0') {
+          m_name[0] = '\0';
+      } else {
+          strcpy(m_name, name);
+      }
+
+      if (destination == nullptr || destination[0] == '\0') {
+          m_destination[0] = '\0';
+      } else {
+          strcpy(m_destination, destination);
+      }
+
+      m_departureYear = 1;
+      m_departureMonth = 7;
+      m_departureDay = 2017;
+  }
+
+  Passenger::~Passenger() {
+
+  }
+
+  // TODO: add the canTravelWith(...) function here
+  bool Passenger::canTravelWith(const Passenger &passenger) const {
+      bool ret =
+          m_departureYear == passenger.m_departureYear && m_departureMonth == passenger.m_departureMonth &&
+          m_departureDay == passenger.m_departureDay && !strcmp(m_destination, passenger.m_destination);
+/*      cout << "year: " << m_departureYear << ", passenger: " << passenger.m_departureYear << endl;
+      cout << "month: " << m_departureMonth << ", passenger: " << passenger.m_departureMonth << endl;
+      cout << "day: " << m_departureDay << ", passenger: " << passenger.m_departureDay << endl;
+      cout << "destination: " << m_destination << ", passenger: " << passenger.m_destination << endl;
+      if (ret) {
+          cout << "cantravelwith returns true for " << passenger.m_name << endl;
+      } else {
+          cout << "cantravelwith returns false for " << passenger.m_name << endl;
+      }*/
+      return ret;
+  }
+
+  // TODO: add the isEmpty() function here
+  bool Passenger::isEmpty() const {
+      bool ret = false;
+      if (this->m_name[0] == '\0' || this->m_destination[0] == '\0' || this->m_departureYear == 0 ||
+          this->m_departureMonth == 0 || this->m_departureDay == 0) {
+          ret = true;
+      }
+      return ret;
+  }
+
+  // below is the member function already provided
+  // TODO: read it and understand how it accomplishes its task
+  void Passenger::display(bool nameOnly) const {
+      if (false == this->isEmpty()) {
+          cout << this->m_name;
+          if (false == nameOnly) {
+              cout << " will travel to " << this->m_destination << ". "
+                   << "The journey will start on "
+                   << this->m_departureYear << "-"
+                   << this->m_departureMonth << "-"
+                   << this->m_departureDay
+                   << "." << endl;
+          }
+      } else {
+          cout << "Invalid passenger!" << endl;
+      }
+  }
+
+
+}
---
+++ OOP-Workshop4/at-home/passenger.h
@@ -0,0 +1,32 @@
+
+//TODO: add header guards here
+#ifndef PASSENGER_H
+#define PASSENGER_H
+
+// TODO: holiday namespace here
+namespace holiday {
+
+  // TODO: declare the class Passenger here
+  class Passenger {
+    // TODO: add the class the attributes,
+    //       the member function already provided,
+    //       and the member functions that you must implement
+    char m_name[32];
+    char m_destination[32];
+    int m_departureYear;
+    int m_departureMonth;
+    int m_departureDay;
+
+  public:
+    Passenger();
+    Passenger(const char* name, const char* destination);
+    Passenger(const char* name, const char* destination);
+    ~Passenger();
+    bool isEmpty() const;
+    bool canTravelWith(const Passenger& passenger) const;
+    void display(bool onlyNames = false) const;
+  };
+
+}
+
+#endif
