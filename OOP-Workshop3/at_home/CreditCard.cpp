bf0cd8316d1aec54f78071f91f1a7803cb097e54
Dominic Christoph Nunes <dcnunes@matrix.senecac.on.ca>
2017-02-13 17:49:37 -0500

    submitted at_home 4

--- OOP-Workshop3/at_home/CreditCard.cpp
+++ OOP-Workshop3/at_home/CreditCard.cpp
@@ -3,80 +3,82 @@
 #include <iostream>
 
 namespace sict {
-using namespace std;
-CreditCard *cc = new CreditCard;
+  using namespace std;
+  CreditCard *cc = new CreditCard;
 
-void CreditCard::initialize(unsigned long long creditCardNumber, int instCode,
-                            int expiryYear, int expiryMonth,
-                            int numberInTheBack) {
+  void CreditCard::initialize(unsigned long long creditCardNumber, int instCode,
+      int expiryYear, int expiryMonth,
+      int numberInTheBack) {
     m_cardNumber = creditCardNumber;
     m_institutionCode = instCode;
     m_expiryYear = expiryYear;
     m_expiryMonth = expiryMonth;
     m_numberInTheBack = numberInTheBack;
     m_numberInTheBack = numberInTheBack;
-}
+  }
 
-void CreditCard::name(char cardHolderName[]) {
+  void CreditCard::name(char cardHolderName[]) {
     strcpy(m_cardHolderName, cardHolderName);
-}
+  }
 
-void CreditCard::write(bool displayName, bool displayCardInfo) const {
-    if (displayName) writeName();
-    if (displayCardInfo) writeCardInfo();
-}
+  void CreditCard::write(bool displayName, bool displayCardInfo) const {
+    if (displayName && displayCardInfo) {
+      writeName();
+      cout << endl;
+      writeCardInfo();
+    } else if (displayName) {
+      writeName();
+    } else if (displayCardInfo) {
+      writeCardInfo();
+    }
+  }
 
-void CreditCard::writeName() const {
-    cout << "Cardholder: " << m_cardHolderName << endl;
-}
+  void CreditCard::writeName() const {
+    cout << "Cardholder: " << m_cardHolderName;
+  }
 
-void CreditCard::writeCardInfo() const {
+  void CreditCard::writeCardInfo() const {
     cout << "Card Number: " << m_cardNumber << endl;
     cout << "Institution: " << m_institutionCode << endl;
-    cout << "Expires: " << m_expiryYear << "/" << m_expiryMonth << endl;
+    cout << "Expires: " << m_expiryMonth << "/" << m_expiryYear << endl;
     cout << "Number at the back: " << m_numberInTheBack;
-}
+  }
 
-bool CreditCard::isValid() {
+  bool CreditCard::isValid() {
     bool nameCheck = false, numCheck = false, instCheck = false,
          yearCheck = false, monthCheck = false, backNumCheck = false,
          finalCheck;
 
     if (strlen(m_cardHolderName)) {
-        nameCheck = true;
+      nameCheck = true;
     }
 
     if (m_cardNumber <= MAX_CARD_NUMBER && m_cardNumber >= MIN_CARD_NUMBER) {
-        numCheck = true;
-        cout << "1 numcheck passed." << endl;
+      numCheck = true;
     }
     if (m_institutionCode <= MAX_INST_NUMBER &&
         m_institutionCode >= MIN_INST_NUMBER) {
-        instCheck = true;
-        cout << "2 instCheck passed." << endl;
+      instCheck = true;
     }
 
     if (m_expiryYear <= MAX_EXP_YEAR && m_expiryYear >= MIN_EXP_YEAR) {
-        yearCheck = true;
-        cout << "3 yearCheck passed." << endl;
+      yearCheck = true;
     }
 
     if (m_expiryMonth <= 12 && m_expiryMonth >= 1) {
-        monthCheck = true;
-        cout << "4 monthCheck passed." << endl;
+      monthCheck = true;
     }
 
     if (m_numberInTheBack % 100 < 100) {
-        backNumCheck = true;
-        cout << "5 backNumCheck passed." << endl;
+      backNumCheck = true;
     }
 
     if (nameCheck && numCheck && instCheck && yearCheck && monthCheck &&
         backNumCheck) {
-        finalCheck = true;
+      finalCheck = true;
     } else {
-        finalCheck = false;
+      finalCheck = false;
     }
     return finalCheck;
-}
+  }
 }
