e6876328feb51dd66b8c2731b05cf959598514e0
domi91c <dominic.n@me.com>
2017-02-22 04:18:26 -0500

    starting ws6. added metadata to in_lab 6 files.

---
+++ OOP-Workshop6/in_lab/Contact.h
@@ -0,0 +1,33 @@
+// OOP244 Workshop 6 in_lab
+// File Contact.h
+// Date 2017-02-21
+// Author Dominic Nunes
+// Student Number 016-183-121
+// Email dcnunes@myseneca.ca
+
+#ifndef SICT_CONTACT_H__
+#define SICT_CONTACT_H__
+
+
+// TODO: add namespace here
+	class Contact
+	{
+		char m_name[21];
+		long long* m_phoneNumbers;
+		int m_noOfPhoneNumbers;
+	public:
+		// TODO: add the default constructor here
+
+		// TODO: add the constructor with parameters here
+
+		// TODO: add the destructor here
+
+		// TODO: add the display function here
+
+		// TODO: add the isEmpty function here
+
+	public:
+		Contact(const Contact& other) = delete;
+		Contact& operator=(const Contact& other) = delete;
+	};
+
